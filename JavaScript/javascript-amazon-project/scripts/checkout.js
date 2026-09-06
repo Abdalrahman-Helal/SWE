cbb2035343fb62b838827aa64b82da5410050320
Abdalrahman Helal <Abdlrhmnhelal@gmail.com>
2026-09-06 16:36:42 +0300

    Create Automated tests for orderSummary

--- JavaScript/javascript-amazon-project/scripts/checkout.js
+++ JavaScript/javascript-amazon-project/scripts/checkout.js
@@ -1,10 +1,10 @@
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
-import { renderOrdreSummary } from './checkout/orderSummary.js';
+import { renderOrderSummary } from './checkout/orderSummary.js';
 
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 
 
 
 renderCheckoutHeader();
-renderOrdreSummary();
+renderOrderSummary();
 renderPaymentSummary();
--- JavaScript/javascript-amazon-project/scripts/checkout/orderSummary.js
+++ JavaScript/javascript-amazon-project/scripts/checkout/orderSummary.js
@@ -10,7 +10,7 @@ import { renderPaymentSummary } from "./paymentSummary.js";
 import { renderCheckoutHeader } from "./checkoutHeader.js";
 
 
-export function renderOrdreSummary() {
+export function renderOrderSummary() {
 
 
   let cartSummaryHTML = '';
@@ -28,7 +28,7 @@ export function renderOrdreSummary() {
 
     cartSummaryHTML +=
       `
-      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+      <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
       Delivery date: ${dateString}
       </div>
@@ -44,7 +44,7 @@ export function renderOrdreSummary() {
       <div class="product-price">
       $${formatCurrency(matchingProduct.priceCents)}
       </div>
-      <div class="product-quantity">
+      <div class="product-quantity js-product-quantity-${matchingProduct.id}">
       <span>
       Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
       </span>
@@ -53,7 +53,8 @@ export function renderOrdreSummary() {
       </span>
       <input class="quantity-input js-quantity-input-${matchingProduct.id}">
       <span class="save quantity-link link-primary js-save-link" data-product-id="${matchingProduct.id}">Save</span>
-      <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+      <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" 
+      data-product-id="${matchingProduct.id}">
       Delete
       </span>
       </div>
@@ -110,7 +111,7 @@ export function renderOrdreSummary() {
       const productId = link.dataset.productId;
       removeFromCart(productId);
       renderCheckoutHeader();
-      renderOrdreSummary();
+      renderOrderSummary();
       renderPaymentSummary();
     });
   });
@@ -166,7 +167,7 @@ export function renderOrdreSummary() {
       element.addEventListener('click', () => {
         const { productId, deliveryOptionId } = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
-        renderOrdreSummary();
+        renderOrderSummary();
         renderPaymentSummary();
       });
     });
