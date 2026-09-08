b64853c14f57ad51c7cd5c5d1abbb53299943a11
Abdalrahman Helal <Abdlrhmnhelal@gmail.com>
2026-09-08 18:25:25 +0300

    Create order using the backend

--- JavaScript/javascript-amazon-project/scripts/checkout/paymentSummary.js
+++ JavaScript/javascript-amazon-project/scripts/checkout/paymentSummary.js
@@ -1,14 +1,16 @@
-import {cart} from '../../data/cart-class.js';
+import { cart } from '../../data/cart-class.js';
 // import {cart} from '../../data/cart.js';
 import { getProduct } from '../../data/products.js';
 import { getDeliveryOption } from '../../data/deliveryOptions.js';
 import { formatCurrency } from '../utils/money.js';
 
+import { addOrder } from '../../data/orders.js';
+
 
 export function renderPaymentSummary() {
   let productPriceCents = 0;
   let shippingPriceCents = 0;
-cart.cartItems.forEach((cartItem) => {
+  cart.cartItems.forEach((cartItem) => {
     const product = getProduct(cartItem.productId);
     productPriceCents += product.priceCents * cartItem.quantity;
     const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
@@ -18,7 +20,7 @@ cart.cartItems.forEach((cartItem) => {
   const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
   const taxCents = totalBeforeTaxCents * 0.1;
   const totalCents = totalBeforeTaxCents + taxCents;
-  
+
   let cartQuantity = 0;
   cart.cartItems.forEach((cartItem) => {
     cartQuantity += cartItem.quantity;
@@ -54,10 +56,30 @@ cart.cartItems.forEach((cartItem) => {
       <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary js-class-order">
       Place your order
     </button>
   `;
 
   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-class-order').addEventListener('click', async () => {
+    try {
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          cart: cart
+        })
+      });
+      const order = await response.json();
+      addOrder(order);
+    } catch (error) {
+      console.log('Unexpected error. Try again later');
+    }
+
+    window.location.href = 'orders.html';
+  });
 }
