1354d1c1345aaaaccaee0a4ef999a3431b8401e0
Abdalrahman Helal <Abdlrhmnhelal@gmail.com>
2026-09-21 22:09:33 +0300

    Add initial SQL script for PostgreSQL database setup

---
+++ 04-PostgreSQL/part1/01-first-database.sql
@@ -0,0 +1,7 @@
+
+-- never do it in prod app 
+-- this is only for learning 
+DROP DATABASE IF EXISTS postgresql_part1;
+
+-- create a new db inside ur postgre server
+CREATE DATABASE postgresql_part1
